0d3f5dd9e69d18ab4e38b61773062aa9e629d8ba
Jason <jason@justchillabit.com>
2018-08-25 20:30:15 -0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="outer-wrapper">
+      <div class="wrapper">
+        <header id="header">
+          <nav id="main-navigation" class="main-nav">
+          <div id="logo-wrapper">
+            <img alt="Jason logo" src="./assets/logo.svg">
+          </div>
+          <div id="nav-wrapper">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+          </div>
+          </nav>
+        </header>
+        <div class="content-inner-wrapper">
+          <div class="main-content-wrapper">
+            <router-view/>
+          </div>
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,7 +32,36 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
+
+.outer-wrapper {
+  padding: 0;
+  width: 1118px;
+  margin: 0 auto;
+  position: relative;
+}
+
+.outer-wrapper>.wrapper {
+  padding: 0;
+  background-color: #fff;
+  position: relative;
+}
+
+#header {
+  position: fixed;
+  top: 0;
+  left: auto;
+  margin: 0 auto;
+  width: 1118px;
+  padding: 40px 0 0;
+  z-index: 1800;
+  background-color: white;
+}
+
+#main-navigation {
+  z-index: 500;
+}
+
+#nav-wrapper {
   padding: 30px;
   a {
     font-weight: bold;
@@ -25,5 +70,13 @@
       color: #42b983;
     }
   }
+}
+
+.content-inner-wrapper {
+  margin-top: 252px;
+}
+
+.content-inner-wrapper>.main-content-wrapper {
+  padding-top: 7%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Jason profile" src="../assets/mischief.png">
+    <div class="main-section-wrapper">
+      <div class="section-title" v-text="description"></div>
+      <div class="section-title" v-text="founded"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data () {
+    return {
+      description: 'UX Design & Development | Startup Founder',
+      founded: 'Founded'
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+
+img {
+  max-width: 40rem;
+}
+
+.main-section-wrapper {
+  
+}
+
+.section-title {
+  border: 1px solid black;
+  padding: .2rem;
+}
+
+</style>
+
