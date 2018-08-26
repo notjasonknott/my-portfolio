e118f31f1fac299aa94208ec5400a4f20c6347ad
Jason <jason@justchillabit.com>
2018-08-26 19:40:22 -0400

    created header component

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,7 @@
 
     <!-- header navbar -->
       <header id="header">
-        <nav id="main-navigation" class="main-nav">
-        <div id="logo-wrapper">
-          <img alt="Jason logo" src="@/assets/logo.svg">
-        </div>
-        <div id="nav-wrapper">
-          <router-link to="/">Home</router-link> |
-          <router-link to="/about">About</router-link>
-        </div>
-        </nav>
+      <header-section/>
       </header>
 
     <!-- content -->
@@ -35,10 +27,12 @@
 
 <script>
 import FooterSection from "./components/Footer.vue";
+import HeaderSection from "./components/Header.vue";
 
 export default {
   components: {
-    FooterSection
+    FooterSection,
+    HeaderSection
   }
 };
 </script>
@@ -70,32 +64,6 @@ body {
   padding: 0;
   background-color: #fff;
   position: relative;
-}
-
-#header {
-  position: fixed;
-  top: 0;
-  left: auto;
-  margin: 0 auto;
-  width: 1118px;
-  padding: 40px 0 0;
-  z-index: 1800;
-  background-color: white;
-}
-
-#main-navigation {
-  z-index: 500;
-}
-
-#nav-wrapper {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 
 .content-inner-wrapper {
